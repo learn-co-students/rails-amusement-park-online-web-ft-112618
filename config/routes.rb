5db7be91d7ed031286e94b0ec590739354083bdc
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-20 12:52:01 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  
+  resources :users, only: %i(new create show update)
+  resources :attractions
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
+  root 'sessions#new'
 end
