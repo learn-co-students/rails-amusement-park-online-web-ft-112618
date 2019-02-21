851704a8701759d906c7021e8887095e2bed5817
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-02-21 11:30:56 -0800

    Done.

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,41 @@
+class AttractionsController < ApplicationController
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def show
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.valid?
+      @attraction.save
+      session[:attraction_id] = @attraction.id
+      redirect_to attraction_path(@attraction)
+    else
+      render 'users/new'
+    end
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating)
+  end
+end
