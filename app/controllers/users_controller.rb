fd941e38c8cb25fc04f4cacbcffa44decc8aac53
Elise Phan <aliceconnic@gmail.com>
2019-02-25 01:24:53 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+   before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+    @message = params[:message] if params[:message]
+    @message ||= false
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user), notice: "Welcome to the theme park!" 
+      else
+        render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        render :edit }
+      end
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+
+    def user_params
+      params.require(:user).permit(:name, :password, :height, :tickets, :happiness, :nausea, :admin)
+    end
+end
