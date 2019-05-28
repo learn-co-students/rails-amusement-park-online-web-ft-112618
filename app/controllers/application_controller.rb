9ef019abc7b2e2882d3610c29cff6fa6dfe65c48
brosie224 <barosengarten@gmail.com>
2019-05-28 14:39:34 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :admin_only
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    redirect_to root_path if !current_user
+  end
+
+  def admin_only
+    unless current_user.admin
+      flash[:notice] = "You must be an admin to perform that function!"
+      redirect_to user_path(current_user)
+    end
+  end
+
 end
